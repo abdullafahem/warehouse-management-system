406c2ca96108e29c71b13357ce89f238d3eb84ff
Abdulla Fahem <abdullaf2019@outlook.com>
2025-02-19 20:21:53 +0100

    Project update

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,8 @@
 <footer class="sticky-footer bg-white">
-    <div class="container my-auto">
-        <div class="copyright text-center my-auto">
+    <div class="my-auto">
+        <div class="copyright text-center my-auto d-flex justify-content-between px-4">
             <span>Copyright &copy; {{ date('Y') }} {{ config('app.name') }}</span>
+            <span class="italic">Theme by <a href="https://startbootstrap.com" target="_blank">Start Bootstrap</a></span>
         </div>
     </div>
 </footer>
